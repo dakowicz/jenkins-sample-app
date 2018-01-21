a833abbaa0dda53e990edbb208b3b9f981803520
Tomasz Dakowicz <tomasz.dakowicz@decerto.pl>
2018-01-21 23:46:32 +0100

    Basic Spring Context setup test

--- jenkins-demo-app/src/test/groovy/com/github/tdakowicz/jenkinsdemo/SpringContextTest.groovy
+++ jenkins-demo-app/src/test/groovy/com/github/tdakowicz/jenkinsdemo/SpringContextTest.groovy
@@ -4,8 +4,8 @@ import com.github.tdakowicz.jenkinsdemo.model.order.domain.Order
 import com.github.tdakowicz.jenkinsdemo.model.order.repo.OrderRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
 
 import javax.persistence.EntityManager
@@ -21,8 +21,15 @@ class SpringContextTest extends Specification {
 	@Autowired
 	OrderRepository orderRepository
 
-	@Transactional
-	def "should setup context"() {
+	@Autowired
+	ApplicationContext applicationContext
+
+	def "should setup spring context"() {
+		expect:
+		applicationContext != null
+	}
+
+	def "should save an order entity"() {
 		given:
 		Order order = new Order()
 		order.description = "ABC"
